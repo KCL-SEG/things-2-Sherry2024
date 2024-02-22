430bc8093cdbf164e78f95ef55f75d9299cdba52
Yiqing Ren <yiqing.1.ren@kcl.ac.uk>
2024-02-22 18:51:43 +0000

    Add ModelForm for Things

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,22 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your forms here.
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description': forms.Textarea()
+            'quantity':forms.NumberInput()
+        }
+    
+    quantity = forms.IntegerField(
+        label = 'Quantity',
+        validators =[MinValueValidator(0),MaxValueValidator(0)]
+    )
+
+
