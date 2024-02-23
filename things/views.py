371f0f7605c183e6e32a7430b944a3281e54f94e
Yiqing Ren <yiqing.1.ren@kcl.ac.uk>
2024-02-23 04:09:36 +0000

    modify

--- things/views.py
+++ things/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from .forms import ThingForm
 
 def home(request):
-    form = ThingForm()
+    #new
+    if request.method == "POST":
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else: 
+        form = ThingForm()
+        # Create a new instance of ThingForm for GET requests or invalid form submissions
     return render(request, 'home.html', {'form': form})
